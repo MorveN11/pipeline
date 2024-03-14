c1ca5dd877e3ff38571e4f51b5617c170aa2e5cf
MorveN11 <morales.patty.jose@gmail.com>
2024-03-14 09:58:13 -0400

    Init commit

---
+++ src/test/java/unix/pipeline/MainTest.java
@@ -0,0 +1,12 @@
+package unix.pipeline;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+  @Test
+  void testMain() {
+    assertEquals(1, 1);
+  }
+}
